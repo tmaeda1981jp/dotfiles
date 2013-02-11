30055bfe284b01e0b8bd6cb490316a110b2b544d
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-02-12 03:12:44 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# emacs
+ln -s `PWD`/emacs $HOME/.emacs.d
+
+# percol
+ln -s `PWD`/percol $HOME/.percol.d
+
+# zsh
+ln -s `PWD`/zsh/.zshrc $HOME/.zshrc
+ln -s `PWD`/zsh/.zshenv $HOME/.zshenv
+
+# vim
+ln -s `PWD`/vim/.vimrc $HOME/.vimrc
+
+# git
+ln -s `PWD`/git/.gitignore $HOME/.gitignore
+ln -s `PWD`/git/.gitconfig $HOME/.gitconfig
+
+# tmux
+ln -s `PWD`/tmux/.tmux.conf $HOME/.tmux.conf
