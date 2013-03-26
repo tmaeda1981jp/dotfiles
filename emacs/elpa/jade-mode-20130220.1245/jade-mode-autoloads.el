089e002707278c86d8398ca279812dbac20e6754
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-03-27 01:54:21 +0900

    Add jade-mode

---
+++ emacs/elpa/jade-mode-20130220.1245/jade-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; jade-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (jade-mode) "jade-mode" "jade-mode.el" (20817 41342))
+;;; Generated autoloads from jade-mode.el
+
+(autoload 'jade-mode "jade-mode" "\
+Major mode for editing jade node.js templates
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("jade-mode-pkg.el") (20817 41342 369746))
+
+;;;***
+
+(provide 'jade-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jade-mode-autoloads.el ends here
---
+++ emacs/inits/20-jade.el
@@ -0,0 +1,6 @@
+;; jade-mode.el
+;; refs: https://github.com/brianc/jade-mode
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
