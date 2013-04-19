2110812e70ab7992b0e6ef2ef259cdc7ed611a98
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-04-19 14:16:52 +0900

    Fix kekybind

--- emacs/inits/30-markdown.el
+++ emacs/inits/30-markdown.el
@@ -6,15 +6,21 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;; C-tがtmuxと重なるのでC-sに変更する
-(define-key markdown-mode-map "\C-c\C-sh" 'markdown-insert-header-dwim)
-(define-key markdown-mode-map "\C-c\C-sH" 'markdown-insert-header-setext-dwim)
-(define-key markdown-mode-map "\C-c\C-s1" 'markdown-insert-header-atx-1)
-(define-key markdown-mode-map "\C-c\C-s2" 'markdown-insert-header-atx-2)
-(define-key markdown-mode-map "\C-c\C-s3" 'markdown-insert-header-atx-3)
-(define-key markdown-mode-map "\C-c\C-s4" 'markdown-insert-header-atx-4)
-(define-key markdown-mode-map "\C-c\C-s5" 'markdown-insert-header-atx-5)
-(define-key markdown-mode-map "\C-c\C-s6" 'markdown-insert-header-atx-6)
-(define-key markdown-mode-map "\C-c\C-s!" 'markdown-insert-header-setext-1)
-(define-key markdown-mode-map "\C-c\C-s@" 'markdown-insert-header-setext-2)
-(define-key markdown-mode-map "\C-c\C-st" 'markdown-insert-header-setext-1)
-(define-key markdown-mode-map "\C-c\C-ss" 'markdown-insert-header-setext-2)
+(add-hook 'markdown-mode-hook
+          (lambda()
+            (local-set-key "\C-c\C-sh" 'markdown-insert-header-dwim)
+            (local-set-key "\C-c\C-sH" 'markdown-insert-header-setext-dwim)
+            (local-set-key "\C-c\C-s1" 'markdown-insert-header-atx-1)
+            (local-set-key "\C-c\C-s2" 'markdown-insert-header-atx-2)
+            (local-set-key "\C-c\C-s3" 'markdown-insert-header-atx-3)
+            (local-set-key "\C-c\C-s4" 'markdown-insert-header-atx-4)
+            (local-set-key "\C-c\C-s5" 'markdown-insert-header-atx-5)
+            (local-set-key "\C-c\C-s6" 'markdown-insert-header-atx-6)
+            (local-set-key "\C-c\C-s!" 'markdown-insert-header-setext-1)
+            (local-set-key "\C-c\C-s@" 'markdown-insert-header-setext-2)
+            (local-set-key "\C-c\C-st" 'markdown-insert-header-setext-1)
+            (local-set-key "\C-c\C-ss" 'markdown-insert-header-setext-2)
+            (local-set-key "\C-c\C-sh" 'markdown-insert-header-dwim)))
+
+(setq markdown-css-path
+      "http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css")
