cd6a58a323b667869c33dcc33da9f1ff2e41beeb
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-05-10 10:29:59 +0900

    Change magit's default diff colors

--- emacs/inits/30-vcs.el
+++ emacs/inits/30-vcs.el
@@ -1,2 +1,10 @@
 (require 'magit)
+
+(eval-after-load 'magit
+  '(progn
+     (set-face-background 'magit-item-highlight "#202020")
+     (set-face-foreground 'magit-diff-add "#40ff40")
+     (set-face-foreground 'magit-diff-del "#ff4040")
+     (set-face-foreground 'magit-diff-file-header "#4040ff")))
+
 (global-set-key "\C-c\C-g" 'magit-status)
