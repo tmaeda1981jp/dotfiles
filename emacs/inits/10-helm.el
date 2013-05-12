21dfcd3d662f057017f5c52f135d443b4252ba0a
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-05-13 02:46:13 +0900

    Add settings for migemo

--- emacs/inits/10-helm.el
+++ emacs/inits/10-helm.el
@@ -1,6 +1,5 @@
 (require 'helm-config)
 (require 'helm-ls-git)
-;; (require 'helm-projectile) ;; 遅いので使わない
 
 (defun my:helm ()
   (interactive)
@@ -9,33 +8,10 @@
      helm-c-source-files-in-current-dir
      helm-c-source-recentf
      helm-c-source-buffers-list
-;;     helm-c-source-ls-git
-;;     helm-c-source-imenu
-;;     helm-c-source-ls-git-status
-;;     helm-c-source-projectile-files-list
      )
    " *helm*"))
 (global-set-key (kbd "C-l") 'my:helm)
 (setq recentf-max-saved-items 15)
-
-;; ---------------------------------------------------------------
-;; helm-c-color-moccur
-;; ref: http://d.hatena.ne.jp/IMAKADO/20080724/1216882563
-;; ---------------------------------------------------------------
-(require 'color-moccur)
-;; 複数の検索語や、特定のフェイスのみマッチなどの機能を有効にする
-(setq moccur-split-word t)
-;; helm-c-moccurの設定
-(require 'helm-c-moccur)
-(setq helm-c-moccur-helm-idle-delay 0.2
-      helm-c-moccur-higligt-info-line-flag t
-      helm-c-moccur-enable-initial-pattern t
-      helm-c-moccur-enable-auto-look-flag t)
-(global-set-key (kbd "C-s") 'helm-c-moccur-occur-by-moccur)
-(global-set-key (kbd "C-M-s") 'helm-c-moccur-dmoccur)
-(add-hook 'dired-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur)))
 
 ;; ---------------------------------------------------------------
 ;; helm-imenu
@@ -48,33 +24,55 @@
 ;; ---------------------------------------------------------------
 (global-set-key (kbd "C-c C-l") 'helm-ls-git-ls)
 
-;;
+;; ---------------------------------------------------------------
 ;; helm-M-x
-;;
+;; ---------------------------------------------------------------
 (global-set-key (kbd "M-x") 'helm-M-x)
 
-;; 
+;; ---------------------------------------------------------------
 ;; bufferを常に画面右側に表示させる為の設定
 ;; refs: http://stackoverflow.com/questions/9992475/how-to-show-anything-buffers-always-in-new-window
+;; ---------------------------------------------------------------
 (setq helm-display-function (lambda (buf)
                               (split-window-horizontally)
                               (other-window 1)
                               (switch-to-buffer buf)
                               ))
 
+;; ---------------------------------------------------------------
 ;; mini-bufferでもC-hが使えるようにする設定
 ;; refs: http://mikio.github.io/article/2013/01/31_helmc-h.html
 ;; refs: http://blog.handlena.me/entry/2013/02/21/110818
+;; ---------------------------------------------------------------
 (eval-after-load 'helm
   '(progn
      (define-key helm-map (kbd "C-h") 'delete-backward-char)))
-(eval-after-load 'helm-c-moccur
-  '(progn
-     (define-key helm-c-moccur-helm-map (kbd "C-h") 'delete-backward-char)))
 
-
+;; ---------------------------------------------------------------
+;; google
+;; ---------------------------------------------------------------
 (setq helm-google-suggest-use-curl-p (executable-find "curl"))
 (setq helm-google-suggest-search-url
       "http://www.google.co.jp/search?hl=ja&num=100&as_qdr=y5&lr=lang_ja&ie=utf-8&oe=utf-8&q=")
 (setq helm-google-suggest-url
       "http://google.co.jp/complete/search?ie=utf-8&oe=utf-8&hl=ja&output=toolbar&q=")
+
+;; ---------------------------------------------------------------
+;; migemo
+;; ---------------------------------------------------------------
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs" "-i" "\g"))
+
+;; Set your installed path
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
+
+;; TODO helm-migemoをrequireするとoccurで絞込みができない．候補が出てこなくなる．
+;; (require 'helm-migemo)
+;; (define-key global-map [(control ?:)] 'helm-migemo)
+(global-set-key (kbd "C-s") 'helm-occur)
