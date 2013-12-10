8927951d12ce32f43056617b6e95a15deebbea5f
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-12-11 02:32:49 +0900

    Add org-capture settings

--- emacs/inits/30-org.el
+++ emacs/inits/30-org.el
@@ -1,12 +1,14 @@
-;;; 
+;;;
 ;;; org-mode
 ;;;
 (require 'org)
 (add-to-list 'auto-mode-alist '("¥¥.org$" . org-mode))
 ;; ~/todoが存在しない場合は以下を評価
 ;; (mkdir "~/todo" t)
-(setq org-default-notes-file "~/todo/mytodo.org")
-(setq org-agenda-files '("~/todo/mytodo.org"))
+(setq org-default-notes-file "~/notes/mytodo.org")
+;(setq org-agenda-files '("~/todo/mytodo.org"))
+(setq org-directory "~/notes")
+(setq org-agenda-files (list org-directory))
 (define-key global-map "\C-cc" 'org-capture)
 (define-key global-map "\C-ca" 'org-agenda)
 
@@ -15,10 +17,12 @@
       '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "SOMEDAY(s)")))
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/todo/mytodo.org" "Tasks")
-         "** TODO %?\n   %i\n")
-        ("m" "Memo" entry (file+headline "~/memo/memo.org" "Memo")
-         "** %?\n   %i\n")))
+      '(("t" "Todo" entry (file+headline "~/notes/mytodo.org" "Tasks")
+         "** TODO %?\n   %i\n  %t\n")
+        ("m" "Memo" entry (file+datetree "~/notes/memo.org" "Memo")
+         "** %?\n  %i\n  %t\n")
+        ("d" "Diary" entry (file+datetree "~/notes/diary.org" "Dialy")
+         "** %U - %^{Activity}  :Diary:")))
 
 ;; CLOSEの時にtimestamp
 (setq org-log-done 'time)
