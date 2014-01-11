9f9400b386f3c7a71e11919df989482e481e538c
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-01-12 02:05:09 +0900

    Update org-capture settings

--- emacs/inits/30-org.el
+++ emacs/inits/30-org.el
@@ -1,8 +1,10 @@
 ;;;
 ;;; org-mode
 ;;;
+
 (require 'org)
 (add-to-list 'auto-mode-alist '("¥¥.org$" . org-mode))
+
 ;; ~/todoが存在しない場合は以下を評価
 ;; (mkdir "~/todo" t)
 (setq org-default-notes-file "~/wiki/notes/mytodo.org")
@@ -20,9 +22,13 @@
       '(("t" "Todo" entry (file+headline "~/wiki/notes/mytodo.org" "Tasks")
          "** TODO %?\n   %i\n  %t\n")
         ("m" "Memo" entry (file+datetree "~/wiki/notes/memo.org" "Memo")
-         "** %?\n  %i\n  %t\n")
+         "** %?\n  %i\n  %t\n" :prepend t)
         ("d" "Diary" entry (file+datetree "~/wiki/notes/diary.org" "Dialy")
-         "** %U - %^{Activity}  :Diary:")))
+         "** %U - %^{Activity} :Diary:\n" :prepend t)
+        ("b" "Book" entry (file+datetree "~/wiki/notes/books.org" "Dialy")
+         "** %U - %^{Activity} :Books:\n" :prepend t)
+        ("v" "Movie" entry (file+datetree "~/wiki/notes/movies.org" "Dialy")
+         "** %U - %^{Activity} :Movies:\n" :prepend t)))
 
 ;; CLOSEの時にtimestamp
 (setq org-log-done 'time)
