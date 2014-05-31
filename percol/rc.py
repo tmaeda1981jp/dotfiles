9069d02eb2e52633a1d8fc3c1c16aafdedc1ccad
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-05-31 23:06:10 +0900

    Fix percol settings

--- percol/rc.py
+++ percol/rc.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python
-# -*- encoding:utf-8 -*-
+from percol.key import SPECIAL_KEYS
+from percol.finder import FinderMultiQueryMigemo, FinderMultiQueryRegex
+
 percol.view.__class__.PROMPT = property(
     lambda self:
-    ur"<bold><blue>QUERY </blue>[a]:</bold> %q" if percol.model.finder.case_insensitive
-    else ur"<bold><green>QUERY </green>[A]:</bold> %q"
+    ur"<bold>Q:</bold> %q" if percol.model.finder.case_insensitive
+    else ur"<bold><green>Q </green>:</bold> %q"
 )
 # Display finder name in RPROMPT
 percol.view.prompt_replacees["F"] = lambda self, **args: self.model.finder.get_name()
 percol.view.RPROMPT = ur"(%F) [%i/%I]"
 
+SPECIAL_KEYS.update({
+    127: '<backspace>'
+})
 percol.import_keymap({
-    "C-h" : lambda percol: percol.command.delete_backward_char(),
-    "C-d" : lambda percol: percol.command.delete_forward_char(),
-    "C-k" : lambda percol: percol.command.kill_end_of_line(),
-    "C-y" : lambda percol: percol.command.yank(),
     "C-a" : lambda percol: percol.command.beginning_of_line(),
     "C-e" : lambda percol: percol.command.end_of_line(),
     "C-b" : lambda percol: percol.command.backward_char(),
     "C-f" : lambda percol: percol.command.forward_char(),
+    "C-d" : lambda percol: percol.command.delete_forward_char(),
+    "C-h" : lambda percol: percol.command.delete_backward_char(),
+    "C-k" : lambda percol: percol.command.kill_end_of_line(),
+    "C-y" : lambda percol: percol.command.yank(),
     "C-n" : lambda percol: percol.command.select_next(),
     "C-p" : lambda percol: percol.command.select_previous(),
     "C-v" : lambda percol: percol.command.select_next_page(),
@@ -27,4 +31,7 @@ percol.import_keymap({
     "C-m" : lambda percol: percol.finish(),
     "C-j" : lambda percol: percol.finish(),
     "C-g" : lambda percol: percol.cancel(),
+    "M-c" : lambda percol: percol.command.toggle_case_sensitive(),
+    "M-m" : lambda percol: percol.command.toggle_finder(FinderMultiQueryMigemo),
+    "M-r" : lambda percol: percol.command.toggle_finder(FinderMultiQueryRegex)
 })
