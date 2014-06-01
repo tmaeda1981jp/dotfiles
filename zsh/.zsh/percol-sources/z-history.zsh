4d894b0e05dc908defa62a58dfc5e8607a6fdfdf
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-01 21:40:37 +0900

    Add percol settings for z command integration

---
+++ zsh/.zsh/percol-sources/z-history.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+function percol-z-history () {
+  local selected=$(z | perl -pne 's{^\d+\.\d+\s+}{}' | percol --query "$LBUFFER")
+  if [ -n "$selected" ]; then
+      BUFFER="cd $selected"
+      zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N percol-z-history
--- zsh/.zsh/percol.zsh
+++ zsh/.zsh/percol.zsh
@@ -5,3 +5,5 @@ for f (~/.zsh/percol-sources/*) source "${f}"
 bindkey '^r' percol-select-history
 bindkey '^x^b' percol-git-recent-branches
 bindkey '^xb' percol-git-recent-all-branches
+bindkey '^x^l' percol-z-history
+
