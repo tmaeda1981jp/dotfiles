f654e1ff297d5dc72e7a9e333671ab74679fbe42
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-03 18:17:23 +0900

    Fix regex

--- zsh/.zsh/percol-sources/z-history.zsh
+++ zsh/.zsh/percol-sources/z-history.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 function percol-z-history () {
-  local selected=$(z | perl -pne 's{^\d+\.\d+\s+}{}' | percol --query "$LBUFFER")
+  local selected=$(z | perl -pne 's{^\d+\.*\d*\s+}{}' | percol --query "$LBUFFER")
   if [ -n "$selected" ]; then
       BUFFER="cd $selected"
       zle accept-line
