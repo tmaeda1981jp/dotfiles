f0c540b13e33037eedbd610db4d21abe5c68b583
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-05 02:42:39 +0900

    Setup flycheck for jslint

--- emacs/inits/30-flycheck.el
+++ emacs/inits/30-flycheck.el
@@ -10,3 +10,17 @@
 (global-set-key "\C-c\C-y" 'list-flycheck-errors)
 
 (setq flycheck-display-errors-delay 0.3)
+
+;; setting for js
+(require 'flycheck)
+(flycheck-define-checker javascript-jslint-reporter
+  "A JavaScript syntax and style checker based on JSLint Reporter.
+
+See URL `https://github.com/FND/jslint-reporter'."
+  :command ("~/.emacs.d/lang/js/jslint-reporter/jslint-reporter" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ":" line ":" column ":" (message) line-end))
+  :modes (js-mode js2-mode js3-mode))
+(add-hook 'js2-mode-hook (lambda ()
+                          (flycheck-select-checker 'javascript-jslint-reporter)
+                          (flycheck-mode)))
