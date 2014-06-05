714372f12ca1dbc40511faa8183d0c89d8b9d6ee
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-06 02:18:44 +0900

    Update haskell-mode setting

--- emacs/inits/20-haskell.el
+++ emacs/inits/20-haskell.el
@@ -14,34 +14,27 @@
  ;; To enable stylish on save.
  '(haskell-stylish-on-save t))
 
-(add-hook 'haskell-mode-hook 'haskell-hook)
-(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
+(autoload 'ghc-init "ghc" nil t)
 
-;; Haskell main editing mode key bindings.
-(defun haskell-hook ()
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (ghc-init)
 
-  ;; interactive-modeのprompt上書き
-  (setq haskell-interactive-prompt ">>> ")
+            ;; Load the current file (and make a session if not already made).
+            (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
+            (define-key haskell-mode-map [f5] 'haskell-process-load-file)
 
-  ;; Use simple indentation.
-  (turn-on-haskell-indentation)
-  (define-key haskell-mode-map (kbd "<return>") 'haskell-simple-indent-newline-same-col)
-  (define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
+            ;; Switch to the REPL.
+            (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
 
-  ;; Load the current file (and make a session if not already made).
-  (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
-  (define-key haskell-mode-map [f5] 'haskell-process-load-file)
+            ;; Jump to the imports. Keep tapping to jump between import
+            ;; groups. C-u f8 to jump back again.
+            (define-key haskell-mode-map [f8] 'haskell-navigate-imports)))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-  ;; Switch to the REPL.
-  (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
-
-  ;; Jump to the imports. Keep tapping to jump between import
-  ;; groups. C-u f8 to jump back again.
-  (define-key haskell-mode-map [f8] 'haskell-navigate-imports))
-
-;; Useful to have these keybindings for .cabal files, too.
-(defun haskell-cabal-hook ()
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
+(add-hook 'haskell-cabal-mode-hook
+          (lambda ()
+            (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+            (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+            (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+            (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)))
