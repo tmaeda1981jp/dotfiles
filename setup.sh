6afe4af476dde35a845e8a55cacc7474145be1a6
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-07 23:25:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,51 +4,55 @@
 if [ -e $HOME/.emacs.d ]; then
     rm -rf $HOME/.emacs.d
 fi
-ln -s `PWD`/emacs $HOME/.emacs.d
+ln -sf `PWD`/emacs $HOME/.emacs.d
 
 # percol
 if [ -e $HOME/.percol.d ]; then
     rm -rf $HOME/.percol.d
 fi
-ln -s `PWD`/percol $HOME/.percol.d
+ln -sf `PWD`/percol $HOME/.percol.d
 
 # vim
-ln -s `PWD`/vim/.vimrc $HOME/.vimrc
+ln -sf `PWD`/vim/.vimrc $HOME/.vimrc
 
 # git
-ln -s `PWD`/git/.gitignore $HOME/.gitignore
-ln -s `PWD`/git/.gitconfig $HOME/.gitconfig
+ln -sf `PWD`/git/.gitignore $HOME/.gitignore
+ln -sf `PWD`/git/.gitconfig $HOME/.gitconfig
 
 # gem
-ln -s `PWD`/gem/.gemrc $HOME/.gemrc
+ln -sf `PWD`/gem/.gemrc $HOME/.gemrc
 
 # tmux
-ln -s `PWD`/tmux/.tmux.conf $HOME/.tmux.conf
+ln -sf `PWD`/tmux/.tmux.conf $HOME/.tmux.conf
 
 # bower
-ln -s `PWD`/bower/.bowerrc $HOME/.bowerrc
+ln -sf `PWD`/bower/.bowerrc $HOME/.bowerrc
 
-# bpython
-ln -s `PWD`/bpython/config $HOME/.config/bpython/config
+# python
+if [ ! -d $HOME/.config/bpython ]; then
+    mkdir -p $HOME/.config/bpython
+fi
+ln -sf `PWD`/python/bpython/config $HOME/.config/bpython/config
+ln -sf `PWD`/python/pylint/.pylintrc $HOME/.pylintrc
 
 # rubocop
-ln -s `PWD`/ruby/rubocop/.rubocop.yml $HOME/.rubocop.yml
+ln -sf `PWD`/ruby/rubocop/.rubocop.yml $HOME/.rubocop.yml
 
 # htmltidy
-ln -s `PWD`/html/.tidyrc $HOME/.tidyrc
+ln -sf `PWD`/html/.tidyrc $HOME/.tidyrc
 
 # ag
-ln -s `PWD`/ag/.agignore $HOME/.agignore
+ln -sf `PWD`/ag/.agignore $HOME/.agignore
 
 # ghci
-ln -s `PWD`/ghci/.ghci $HOME/.ghci
+ln -sf `PWD`/ghci/.ghci $HOME/.ghci
 
 # zsh
-ln -s `PWD`/zsh/.zshrc $HOME/.zshrc
-ln -s `PWD`/zsh/.zshenv $HOME/.zshenv
+ln -sf `PWD`/zsh/.zshrc $HOME/.zshrc
+ln -sf `PWD`/zsh/.zshenv $HOME/.zshenv
 
 # percol
 if [ -e $HOME/.zsh ]; then
     rm -rf $HOME/.zsh
 fi
-ln -s `PWD`/zsh/.zsh $HOME/.zsh
+ln -sf `PWD`/zsh/.zsh $HOME/.zsh
