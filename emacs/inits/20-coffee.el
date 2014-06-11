35e2f8b7230c78f2b1c4d477694c01671556d738
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-11 16:25:39 +0900

    Add coffee-mode setting

---
+++ emacs/inits/20-coffee.el
@@ -0,0 +1,3 @@
+(autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
