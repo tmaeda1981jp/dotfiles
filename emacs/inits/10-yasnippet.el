0661e9c9a9988799d9104275671bba81b1160ecb
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-27 15:52:26 +0900

    Fix snippet path

--- emacs/inits/10-yasnippet.el
+++ emacs/inits/10-yasnippet.el
@@ -6,7 +6,8 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets" ;; personal collection
-        "~/.emacs.d/elpa/yasnippet-20130218.2229/snippets" ;; default collection
+        "~/.emacs.d/elpa/yasnippet-20140617.1640/snippets" ;; default collection
         ))
 (yas-global-mode 1)
 (custom-set-variables '(yas-trigger-key "TAB"))
+
