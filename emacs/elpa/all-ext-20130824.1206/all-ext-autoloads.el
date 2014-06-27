dba30377c95055d6850680358ad4ad128fb0b993
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-06-27 18:16:37 +0900

    Add all.el

---
+++ emacs/elpa/all-ext-20130824.1206/all-ext-autoloads.el
@@ -0,0 +1,18 @@
+;;; all-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("all-ext-pkg.el" "all-ext.el") (21421
+;;;;;;  9585 278974 0))
+
+;;;***
+
+(provide 'all-ext-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; all-ext-autoloads.el ends here
---
+++ emacs/elpa/all-ext-20130824.1206/all-ext-pkg.el
@@ -0,0 +1,1 @@
+(define-package "all-ext" "20130824.1206" "Extension of M-x all" (quote ((all "1.0"))))
