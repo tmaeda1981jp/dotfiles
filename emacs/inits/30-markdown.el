923cdc3dd03cee8541f4fcba5db88e189babdec8
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-07-01 18:44:45 +0900

    Update 30-markdown.el

--- emacs/inits/30-markdown.el
+++ emacs/inits/30-markdown.el
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="generator" content="pandoc">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-  <title></title>
-  <style type="text/css">code{white-space: pre;}</style>
-  <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-  <link rel="stylesheet" href="/Users/tmaeda/.emacs.d/lang/css/github.css">
-</head>
-<body>
-<p>(defvar css-file-path &quot;~/.emacs.d/lang/css/github.css&quot;) (defun gfm-to-html () &quot;Convert GFM(Github Flavored Markdown) to HTML.&quot; (let ((gfm buffer-file-name) (html (replace-regexp-in-string &quot;.md$&quot; &quot;.html&quot; buffer-file-name))) (shell-command-to-string (concat &quot;pandoc &quot; &quot;-s &quot; &quot;-t &quot; &quot;html5 &quot; &quot;-c &quot; css-file-path &quot; -o &quot; html &quot; &quot; gfm))))</p>
-<p>(add-hook 'markdown-mode-hook '(lambda () (add-hook 'after-save-hook 'gfm-to-html)))</p>
-</body>
-</html>
+(defvar css-file-path "~/.emacs.d/lang/css/github.css")
+
+(defun gfm-to-html ()
+  "Convert GFM(Github Flavored Markdown) to HTML."
+  (if (and (string-match (rx-to-string `(: ,"md" eos) t)
+                         buffer-file-name) t)
+      (let* ((gfm buffer-file-name)
+             (html (replace-regexp-in-string "\.md$" ".html" buffer-file-name)))
+
+        (shell-command-to-string
+         (mapconcat 'identity
+                    (list "pandoc" "-s" "-t" "html5" "-c" css-file-path "-o" html gfm) " ")))
+    ))
+
+
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (add-hook 'after-save-hook 'gfm-to-html)))
