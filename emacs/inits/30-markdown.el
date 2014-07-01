c2f36ffcc6d4ca30efc4137f18f1ba4ff5ce6dfa
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-07-01 18:59:31 +0900

    Fix exported file path

--- emacs/inits/30-markdown.el
+++ emacs/inits/30-markdown.el
@@ -1,17 +1,18 @@
 (defvar css-file-path "~/.emacs.d/lang/css/github.css")
+(defvar tmp-file-dir "~/tmp")
 
 (defun gfm-to-html ()
   "Convert GFM(Github Flavored Markdown) to HTML."
   (if (and (string-match (rx-to-string `(: ,"md" eos) t)
                          buffer-file-name) t)
       (let* ((gfm buffer-file-name)
-             (html (replace-regexp-in-string "\.md$" ".html" buffer-file-name)))
+;;             (html-file-name (replace-regexp-in-string "\.md$" ".html" buffer-file-name))
+             (tmp-file-path (format "%s/tmp.html" tmp-file-dir)))
 
         (shell-command-to-string
          (mapconcat 'identity
-                    (list "pandoc" "-s" "-t" "html5" "-c" css-file-path "-o" html gfm) " ")))
+                    (list "pandoc" "-s" "-t" "html5" "-c" css-file-path "-o" tmp-file-path gfm) " ")))
     ))
-
 
 (add-hook 'markdown-mode-hook
           '(lambda ()
