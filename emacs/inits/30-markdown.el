5707f27dbfddadea9308161bce97b29121e7180f
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-07-05 15:03:13 +0900

    Create function to preview html which pandoc create on save.

--- emacs/inits/30-markdown.el
+++ emacs/inits/30-markdown.el
@@ -1,19 +1,24 @@
 (defvar css-file-path "~/.emacs.d/lang/css/github.css")
-(defvar tmp-file-dir "~/tmp")
+(defvar tmp-file-dir "$HOME/tmp")
+(defvar tmp-file-path (format "%s/tmp.html" tmp-file-dir))
 
 (defun gfm-to-html ()
   "Convert GFM(Github Flavored Markdown) to HTML."
   (if (and (string-match (rx-to-string `(: ,"md" eos) t)
                          buffer-file-name) t)
-      (let* ((gfm buffer-file-name)
-;;             (html-file-name (replace-regexp-in-string "\.md$" ".html" buffer-file-name))
-             (tmp-file-path (format "%s/tmp.html" tmp-file-dir)))
-
+      (let* ((gfm buffer-file-name))
         (shell-command-to-string
          (mapconcat 'identity
-                    (list "pandoc" "-s" "-t" "html5" "-c" css-file-path "-o" tmp-file-path gfm) " ")))
-    ))
+                    (list "pandoc" "-s" "-t" "html5" "-c" css-file-path "-o" tmp-file-path gfm) " ")))))
+
+(defun preview-on-browser ()
+  "Preview html on browser"
+  (interactive)
+  (shell-command-to-string
+   (mapconcat 'identity
+              (list "open" "-a" "Google\\ Chrome" tmp-file-path) " ")))
 
 (add-hook 'markdown-mode-hook
           '(lambda ()
+             (define-key markdown-mode-map (kbd "C-c p") 'preview-on-browser)
              (add-hook 'after-save-hook 'gfm-to-html)))
