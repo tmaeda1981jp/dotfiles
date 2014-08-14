e61bb72bafb474a9a03178f6564b5bad207760d7
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-08-14 21:10:03 +0900

    Add shortcut keys for css-mode/sass-mode

--- emacs/inits/20-css.el
+++ emacs/inits/20-css.el
@@ -3,6 +3,10 @@
 (require 'helm-css-scss)
 (setq helm-css-scss-insert-close-comment-depth 2)
 
+(add-hook 'css-mode-hook
+          (lambda ()
+            (local-set-key "\C-r" 'helm-css-scss)))
+
 ;; sass
 
 ;; refs: http://qiita.com/sawamur@github/items/bb50d84af4d01a2eb5c2
@@ -15,6 +19,7 @@
   (and
    (set (make-local-variable 'css-indent-offset) 2)
    (set (make-local-variable 'scss-compile-at-save) nil)
+   (local-set-key "\C-r" 'helm-css-scss)
    )
   )
 (add-hook 'scss-mode-hook
