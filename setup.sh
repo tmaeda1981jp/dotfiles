847e1bb968e1c0865e7ae6416e236691f5ac7c3a
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-01-01 14:33:53 +0900

    Update setup scripts

--- setup.sh
+++ setup.sh
@@ -1,16 +1,108 @@
 #!/bin/bash
 
+# zsh
+ln -sf `PWD`/zsh/.zshrc $HOME/.zshrc
+ln -sf `PWD`/zsh/.zshenv $HOME/.zshenv
+
+# anyenv
+if [ ! -d ${HOME}/.anyenv ] ; then
+    git clone https://github.com/riywo/anyenv ~/.anyenv
+
+    exec $SHELL -l
+
+    anyenv install rbenv
+    anyenv install pyenv
+    anyenv install ndenv
+
+    exec $SHELL -l
+
+    # rbenv
+    rbenv install 2.1.0
+    rbenv global 2.1.0
+
+    # pyenv
+    pyenv install 2.7.8
+    pyenv global 2.7.8
+
+    # ndenv
+    ndenv install 0.10.35
+    ndenv global 0.10.35
+
+    #gem
+    gems=(
+        bundler
+        brewdler
+        tmuxinator
+        jekyll
+        middleman
+        compass
+        sass
+        rails
+        rspec
+    )
+    for gem in ${gems[@]};
+    do
+        gem install ${gem} --no-ri --no-rdoc
+    done
+    rbenv rehash
+
+    # pip
+    pip install percol ansible virtualenv django
+    pyenv rehash
+
+    # npm
+    npm install bower grunt-cli volo jade mocha browserify jslint jshint gulp yo strongloop
+    ndenv rehash
+else
+    echo -e "\033[1m[SKIPPED]\033[0m anyenv has been already installed"
+fi
+
+# 時間がかかるのであとでうまくやる方法を考える
+# とりあえずanyenvと一緒にやる
+
+# gem
+# gems=(
+#     bundler
+#     brewdler
+#     tmuxinator
+#     jekyll
+#     middleman
+#     compass
+#     sass
+#     rails
+#     rspec
+# )
+# for gem in ${gems[@]};
+# do
+#     gem install ${gem} --no-ri --no-rdoc
+# done
+# rbenv rehash
+
+# # pip
+# pip install percol ansible virtualenv django
+# pyenv rehash
+
+# # npm
+# npm install bower grunt-cli volo jade mocha browserify jslint jshint gulp yo strongloop
+# ndenv rehash
+
+
+# homebrew
+if [ ! -x /usr/local/bin/brew ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo -e "\033[1m[SKIPPED]\033[0m Homebrew has been already installed"
+fi
+
 # brewfile
-if which brewdle > /dev/null; then
+if [ ! -x $HOME/.anyenv/envs/rbenv/shims/brewdle ]; then
     ln -sf `PWD`/osx/Brewfile $HOME/Brewfile
     cd $HOME
     brewdle install
     brew cask alfred link
     cd $HOME/dotfiles
 else
-    echo "============"
-    echo "Skip brewdle"
-    echo "============"
+    echo -e "\033[1m[SKIPPED]\033[0m Brewdler has been already installed"
 fi
 
 # emacs
@@ -18,6 +110,7 @@ if [ -e $HOME/.emacs.d ]; then
     rm -rf $HOME/.emacs.d
 fi
 ln -sf `PWD`/emacs $HOME/.emacs.d
+ln -sf /usr/local/opt/cask $HOME/.cask
 cd $HOME/dotfiles/emacs
 cask install
 cd $HOME/dotfiles
@@ -74,10 +167,6 @@ ln -sf `PWD`/ag/.agignore $HOME/.agignore
 
 # ghci
 ln -sf `PWD`/ghci/.ghci $HOME/.ghci
-
-# zsh
-ln -sf `PWD`/zsh/.zshrc $HOME/.zshrc
-ln -sf `PWD`/zsh/.zshenv $HOME/.zshenv
 
 # percol
 if [ -e $HOME/.zsh ]; then
