82df11d8678d639ebd38cac538946c43baec5047
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-10-04 23:28:14 +0900

    Update quickrun settings

--- emacs/inits/30-quickrun.el
+++ emacs/inits/30-quickrun.el
@@ -4,5 +4,5 @@
 ;; http://d.hatena.ne.jp/syohex/20111201/1322665378
 ;;
 (require 'quickrun)
-(global-set-key (kbd "<f4>") 'quickrun)
+(global-set-key (kbd "C-c p") 'quickrun)
 (global-set-key "\C-ce" 'helm-quickrun)
