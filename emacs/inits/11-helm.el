9eee9e70b56282926e013daba54c6ccd3659987e
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-10-09 10:51:38 +0900

    Fix setting for helm-migemo

--- emacs/inits/11-helm.el
+++ emacs/inits/11-helm.el
@@ -101,9 +101,24 @@
 (load-library "migemo")
 (migemo-init)
 
-;; TODO helm-migemoをrequireするとoccurで絞込みができない．候補が出てこなくなる．
-;; (require 'helm-migemo)
-;; (define-key global-map [(control ?:)] 'helm-migemo)
+;; ref: http://rubikitch.com/2014/12/19/helm-migemo/
+(require 'helm-migemo)
+(with-eval-after-load "helm-migemo"
+  (defun helm-compile-source--candidates-in-buffer (source)
+    (helm-aif (assoc 'candidates-in-buffer source)
+        (append source
+                `((candidates
+                   . ,(or (cdr it)
+                          (lambda ()
+                            ;; Do not use `source' because other plugins
+                            ;; (such as helm-migemo) may change it
+                            (helm-candidates-in-buffer (helm-get-current-source)))))
+                  (volatile) (match identity)))
+      source))
+  ;; [2015-09-06 Sun]helm-match-plugin -> helm-multi-match変更の煽りを受けて
+  (defalias 'helm-mp-3-get-patterns 'helm-mm-3-get-patterns)
+  (defalias 'helm-mp-3-search-base 'helm-mm-3-search-base))
+
 (require 'all-ext)
 (global-set-key (kbd "C-c s") 'helm-occur)
 (define-key helm-map (kbd "C-c C-a") 'all-from-helm-occur)
