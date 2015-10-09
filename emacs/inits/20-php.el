66c441e8cdd9607bb3c34245d9eb083de6a242f5
tmaeda1981jp <tmaeda1981jp@gmail.com>
2015-10-10 03:00:24 +0900

    Fix indent settings for php-mode

--- emacs/inits/20-php.el
+++ emacs/inits/20-php.el
@@ -4,11 +4,27 @@
 (require 'php-mode)
 (scroll-bar-mode -1)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
 (add-hook 'php-mode-hook
-          (lambda ()
-            (setq flycheck-phpcs-standard "PSR2")
-            (c-set-offset 'case-label 4)
-            (c-set-offset 'arglist-intro 4)
-            (c-set-offset 'arglist-cont-nonempty 4)
-            (global-set-key (kbd "C-c C-l") 'helm-ls-git-ls)
-            (c-set-offset 'arglist-close 0)))
+  (lambda ()
+    (setq flycheck-phpcs-standard "PSR2")
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+
+
+;; (add-hook 'php-mode-hook
+;;           (lambda ()
+;;             (setq flycheck-phpcs-standard "PSR2")
+;;             (c-set-offset 'case-label 4)
+;;             (c-set-offset 'arglist-intro 4)
+;;             (c-set-offset 'arglist-cont-nonempty 4)
+;;             (global-set-key (kbd "C-c C-l") 'helm-ls-git-ls)
+;;             (c-set-offset 'arglist-close 0)))
