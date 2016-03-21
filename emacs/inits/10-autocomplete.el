2b9d3effe1d1269825479818f89fcfbd14b4d234
tmaeda1981jp <tmaeda1981jp@gmail.com>
2016-03-21 17:06:00 +0900

    Update typescript setting

--- emacs/inits/10-autocomplete.el
+++ emacs/inits/10-autocomplete.el
@@ -2,7 +2,10 @@
 ;; auto-complete
 ;;
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20130330.1836/dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/.cask/24.5.1/elpa/auto-complete-20160229.1706/dict")
 (ac-config-default)
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+;; company
+(require 'company)
--- emacs/inits/20-ts.el
+++ emacs/inits/20-ts.el
@@ -1,6 +1,19 @@
 ;; ---------------------------------------------------------------------------------------------------------------
 ;; typescript-mode
-;; refs: http://blogs.msdn.com/b/interoperability/archive/2012/10/01/sublime-text-vi-emacs-typescript-enabled.aspx
+;; old - ;; refs: http://blogs.msdn.com/b/interoperability/archive/2012/10/01/sublime-text-vi-emacs-typescript-enabled.aspx
+;; refs: http://blog.shibayu36.org/entry/2015/07/30/165626
 ;; ---------------------------------------------------------------------------------------------------------------
+(require 'typescript-mode)
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-(autoload 'typescript-mode "TypeScript" "Major mode for editing typescript." t)
+;;(autoload 'typescript-mode "TypeScript" "Major mode for editing typescript." t)
+
+;; refs: https://github.com/ananthakumaran/tide
+(require 'tide)
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode t)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode t)
+            (company-mode-on)))
+
