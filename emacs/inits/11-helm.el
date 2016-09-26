9db0d4db6153e384ad769d9477fb9f9a3ea37c3c
tmaeda1981jp <tmaeda1981jp@gmail.com>
2016-09-27 02:57:55 +0900

    Fix to use helm-migemo

--- emacs/inits/11-helm.el
+++ emacs/inits/11-helm.el
@@ -92,24 +92,7 @@
 (setq migemo-coding-system 'utf-8-unix)
 (load-library "migemo")
 (migemo-init)
-
-;; ref: http://rubikitch.com/2014/12/19/helm-migemo/
-;; (require 'helm-migemo)
-;; (with-eval-after-load "helm-migemo"
-;;   (defun helm-compile-source--candidates-in-buffer (source)
-;;     (helm-aif (assoc 'candidates-in-buffer source)
-;;         (append source
-;;                 `((candidates
-;;                    . ,(or (cdr it)
-;;                           (lambda ()
-;;                             ;; Do not use `source' because other plugins
-;;                             ;; (such as helm-migemo) may change it
-;;                             (helm-candidates-in-buffer (helm-get-current-source)))))
-;;                   (volatile) (match identity)))
-;;       source))
-;;   ;; [2015-09-06 Sun]helm-match-plugin -> helm-multi-match変更の煽りを受けて
-;;   (defalias 'helm-mp-3-get-patterns 'helm-mm-3-get-patterns)
-;;   (defalias 'helm-mp-3-search-base 'helm-mm-3-search-base))
+(helm-migemo-mode 1)
 
 (require 'all-ext)
 (global-set-key (kbd "C-c s") 'helm-occur)
