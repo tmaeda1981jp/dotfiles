29c371128413eba29a31a7df2a602d9dbf6a83a9
tmaeda1981jp <tmaeda1981jp@gmail.com>
2019-04-12 02:18:25 +0900

    Change keychord key bind

--- emacs/inits/01-keychord.el
+++ emacs/inits/01-keychord.el
@@ -5,6 +5,6 @@
       key-chord-safety-interval-forward  0.25)
   (key-chord-mode t)
   :config
-  (key-chord-define-global ";r" 'replace-string)
-  (key-chord-define-global "'r" 'replace-regexp)
+  (key-chord-define-global ";s" 'replace-string)
+  (key-chord-define-global ";r" 'replace-regexp)
   )
